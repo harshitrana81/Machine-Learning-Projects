3b4891bac79856074ad62236dffcee72f71ad18f
harshitrana81 <ranaharshit269@gmail.com>
2026-02-24 00:28:38 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -8,7 +8,6 @@ Original file is located at
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
 def analyze_and_forecast():
